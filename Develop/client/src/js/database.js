1d77161980db4c4b15eee20bce707952a832b6d4
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-07-25 19:21:13 -0400

    adding a . instead of a comma on install.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {console.error('putDb not implemented');
 const contactDb = await openDB('jate', 1);
 // Create a new transaction and specify the database and data privileges.
@@ -26,7 +26,7 @@ const request = store.add({ content });
 const result = await request;
   console.log('🚀 - data saved to the database', result);
 }
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {console.error('getDb not implemented');
 const contactDb = await openDB('jate', 1);
 
